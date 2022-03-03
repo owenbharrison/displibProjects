3ddc9115814bdc40151638ab9d1f405026308238
Owen Harrison <owenbharrison@gmail.com>
2022-03-02 19:12:15 -0700

    made softbody faster

--- softbody2d/src/main.cpp
+++ softbody2d/src/main.cpp
@@ -3,20 +3,38 @@
 #include "Softbody.h"
 using namespace displib;
 
+struct Stopwatch {
+	std::chrono::steady_clock::time_point begin, end;
+
+	void start() {
+		begin=std::chrono::steady_clock::now();
+	}
+
+	void stop() {
+		end=std::chrono::steady_clock::now();
+	}
+
+	float duration() {
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+	}
+};
+
 class Demo : public Engine {
 	public:
 	V2D grav;
 	AABB2D bounds, mouseBox;
 	Softbody jello, ball, cloth;
+	float totalUpdateTime=0;
+	Stopwatch watch;
 
 	void setup() override {
 		grav=V2D(0.0f, 21.0f);
 
 		float stiff=240.47f;
 		float damp=6.23f;
-		jello.formRectangle(7, 7, AABB2D(width*0.1f, height*0.25f, width*0.3f, height*0.75f), stiff, damp);
-		ball.formEllipse(3, 9, AABB2D(width*0.7f, height*0.4f, width*0.85f, height*0.7f), stiff*1.4, damp);
-		cloth.formCloth(12, 6, AABB2D(width*0.3f, 0, width*0.7f, height*0.4f), stiff, damp);
+		jello.formRectangle(6, 7, AABB2D(width*0.1f, height*0.25f, width*0.3f, height*0.75f), stiff, damp);
+		ball.formEllipse(4, 10, AABB2D(width*0.7f, height*0.4f, width*0.85f, height*0.7f), stiff*1.4, damp);
+		cloth.formCloth(16, 8, AABB2D(width*0.3f, 0, width*0.7f, height*0.4f), stiff, damp);
 
 		bounds=AABB2D(0, 0, width, height);
 		updateMouseBox();
@@ -64,15 +82,12 @@ class Demo : public Engine {
 
 		//draw springs
 		rst.setChar('j');
-		rst.setColor(Raster::GREEN);
 		jello.renderSprings(rst);
 
 		rst.setChar('b');
-		rst.setColor(Raster::RED);
 		ball.renderSprings(rst);
 
 		rst.setChar('c');
-		rst.setColor(Raster::GREY);
 		cloth.renderSprings(rst);
 
 		//draw particles
